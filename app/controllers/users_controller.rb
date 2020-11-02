0054b331f018a0257bfa2eaf52828e0fb9e1a836
sinework <sine.s.amare@gmail.com>
2020-11-02 23:30:51 +0300

    Update: update the password field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
-  # def create
-  #   @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
-  #   if @user.save
-  #     redirect_to new_user_path
-  #   else
-  #     render :new
-  #   end
-  # end
-
+  
   def create
     @user = User.new(user_params)
 
