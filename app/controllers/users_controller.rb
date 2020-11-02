cf21946896e65f75f74ab00ef54da914cc6ca3cd
sinework <sine.s.amare@gmail.com>
2020-11-02 16:06:26 +0300

    Update: Fix rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
   include UsersHelper
   def new
-    @user= User.new
+    @user = User.new
   end
-  def create
-      @user = User.new(user_params)
 
-      if @user.save
-          redirect_to new_user_path
-        else
-          redirect_to new_user_path, alert: "Error creating user."
-        end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      redirect_to new_user_path, alert: 'Error creating user.'
+    end
   end
   # def create
   #   @user = User.new(username: params[:username], email: params[:email], password: params[:password])
@@ -26,12 +27,11 @@ class UsersController < ApplicationController
   end
 
   def update
-  @user = User.find(params[:id])
- 
-  if @user.update(user_params)
-    redirect_to edit_user_path
+    @user = User.find(params[:id])
+
+    redirect_to edit_user_path if @user.update(user_params)
   end
-end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
