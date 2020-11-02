983697b6333fd29f1f9cca59118d4724db1379d1
sinework <sine.s.amare@gmail.com>
2020-11-02 16:02:51 +0300

    Add: Add edit user method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,17 @@ class UsersController < ApplicationController
   #     redirect_to new_user_path, alert: 'Error creating user.'
   #   end
   # end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  @user = User.find(params[:id])
+ 
+  if @user.update(user_params)
+    redirect_to edit_user_path
+  end
+end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
