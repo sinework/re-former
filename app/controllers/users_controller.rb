598365a4c6567772eb56827171d82b0976329a46
sinework <sine.s.amare@gmail.com>
2020-11-02 10:49:45 +0300

    Update: Nest the user params into a hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
+    include UsersHelper
     def new
 
     end
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
 
         if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end
+            redirect_to new_user_path
+          else
+            redirect_to new_user_path, alert: "Error creating user."
+          end
     end
 end
